444dc102280534570d86f5e0e583213901aaea81
Kesigomon <kesigomon@gmail.com>
2022-03-03 12:06:41 +0900

    トップページ書いた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,23 @@ import * as React from "react"
 
 // markup
 const IndexPage = () => {
-  return (
-    <div>
-      役職パネルのドキュメントです<br/>
-      フロントできる人だれかいい感じのトップページ書いてくれ
-    </div>
-  )
+    return (
+        <div>
+            役職パネルのドキュメントです<br/>
+            <h1>役職パネルとは？</h1>
+            サーバーメンバーが自由につけられる役職の付与・解除を自動化するBOTです。<br/>
+            サーバーの管理者がこのBOTを利用して「役職パネル」を作成し、メンバーはそのパネルを確認してリアクションを行います。
+            すると、そのリアクションに対応した役職をBOTが自動で付与してくれます。
+            <h1>招待リンク</h1>
+            <a href={"https://discord.com/api/oauth2/authorize?client_id=895912135039803402&permissions=268782656&scope=bot%20applications.commands"}>
+                Add to Your Server
+            </a>
+            <h1>サポートサーバー</h1>
+            <a href={"https://discord.gg/CjgwJGC3uv"}>
+                https://discord.gg/CjgwJGC3uv
+            </a>
+        </div>
+    )
 }
 
 export default IndexPage
